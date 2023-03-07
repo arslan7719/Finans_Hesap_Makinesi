755ca9eb4ab8c6c504d41b37b0dde1377cc321c8
arslan7719 <95647825+arslan7719@users.noreply.github.com>
2023-03-07 13:59:39 +0300

    Create Faiz_Hesabi.py

---
+++ Aşama_1/Faiz_Hesabi.py
@@ -0,0 +1,17 @@
+# giriş hesap türü sorma fakat bunu sonra ekle 
+
+# Basit faiz işlemleri
+x=int(input("Anapara'yı belirtiniz:"))
+y=int(input("Faiz işleme süresini belirtiniz:"))
+z=float(input("Faiz oranını belirtiniz (0.05 halinde):"))
+
+basit_faiz=x*(1+(z*y))
+
+print("Basit faiz tutarı:",basit_faiz)
+
+# bileşik Faiz işlemleri
+
+""" atamalar girişde yapıldığı için direk formule geçildi  """
+
+bilesik_faiz=x*((1+z)**y)
+print("Bileşik Faiz Tutarı:",bilesik_faiz)
